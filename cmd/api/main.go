124ffc48c361378e7992b3bf56c07decf48bcc09
gangsaur <andrihardono21@gmail.com>
2026-05-07 14:09:23 +0700

    [Feature] Start simple web server in main with configurable port and open /h for health check

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -2,6 +2,9 @@ package main
 
 import (
 	"log"
+	"net/http"
+
+	"gangsaur.com/billing-exercise/internal/server"
 
 	"github.com/joho/godotenv"
 )
@@ -11,4 +14,12 @@ func main() {
 	if err != nil {
 		log.Fatalf("Error loading .env file: %s", err.Error())
 	}
+
+	apiServer := server.NewApiServer()
+
+	log.Printf("Listening at %v...", apiServer.GetServerAddress())
+	if err := apiServer.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+		log.Fatalf("Server err: %s", err.Error())
+	}
+	log.Printf("Exiting...")
 }
---
+++ internal/server/api.go
@@ -0,0 +1,64 @@
+package server
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+	"os/signal"
+)
+
+type ApiServer struct {
+	server *http.Server
+}
+
+func NewApiServer() *ApiServer {
+	// Init mux and server
+	mux := createMux()
+
+	port := os.Getenv("API_PORT")
+	if port == "" {
+		port = "9000" // Simply default to 9000
+	}
+
+	srv := http.Server{
+		Addr:    fmt.Sprintf(":%s", port),
+		Handler: mux,
+	}
+
+	c := make(chan os.Signal, 1)
+	signal.Notify(c, os.Interrupt)
+	go func() {
+		for sig := range c {
+			log.Printf("Received %s, shutting down", sig)
+			err := srv.Shutdown(context.Background())
+			if err != nil {
+				log.Printf("Error when shutting down server: %s", err.Error())
+			}
+		}
+	}()
+
+	return &ApiServer{
+		server: &srv,
+	}
+}
+
+func (s ApiServer) GetServerAddress() string {
+	return s.server.Addr
+}
+
+func (s ApiServer) ListenAndServe() error {
+	return s.server.ListenAndServe()
+}
+
+func createMux() *http.ServeMux {
+	mux := http.NewServeMux()
+
+	mux.HandleFunc("GET /h", func(w http.ResponseWriter, _ *http.Request) {
+		w.WriteHeader(http.StatusOK)
+		_, _ = w.Write([]byte("OK"))
+	})
+
+	return mux
+}
